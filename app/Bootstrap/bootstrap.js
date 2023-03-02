a26c33fd10fd616cc226fada34c41622b66ab733
shivansh-cg <shivansh@cognitivegarage.ai>
2023-03-02 16:33:28 +0530

    fixes

--- app/Bootstrap/bootstrap.js
+++ app/Bootstrap/bootstrap.js
@@ -3,17 +3,17 @@ const Mongo = require('../Initializers/Mongo')
 intializeServices = () => {
   const promiseStack = [];
 
-  const mongoPromise = new Promise((resolve, reject) => {
-    try {
-      // Mongo.connectToServer(() => {
-      //   resolve();
-      // })
-    } catch (error) {
-      reject(new Error('Unable to create connection with mongo, please make sure mongo server is running.'));
-    }
-  });
+  // const mongoPromise = new Promise((resolve, reject) => {
+  //   try {
+  //     // Mongo.connectToServer(() => {
+  //     //   resolve();
+  //     // })
+  //   } catch (error) {
+  //     reject(new Error('Unable to create connection with mongo, please make sure mongo server is running.'));
+  //   }
+  // });
 
-  // promiseStack.push(mongoPromise);
+  promiseStack.push(Mongo.connectToServer());
 
   return promiseStack;
 
